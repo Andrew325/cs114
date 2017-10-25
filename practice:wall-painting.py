7ff06775056dd91657f0c432f2e693b90bf3c7b6
s203 <s203@ai241lab0203m06.aipdcls.edu>
2017-10-25 12:03:39 -0700

    updates

--- practice:wall-painting.py
+++ practice:wall-painting.py
@@ -3,4 +3,9 @@ width = int(input())
 print('How high is the wall?')
 height = int(input())
 print('How much is a gallon of paint?')
-price = int(input)
+price = int(input())
+gallon = 400
+wall_size = width * height
+wall_size = wall_size // gallon
+cost = price * wall_size
+print('It will cost $' + str(cost))
