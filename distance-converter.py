0f03149893f65d8489deec9b41f533030e8074b2
s203 <s203@AI241LAB0203M06.local>
2017-11-08 10:19:11 -0800

    updates

--- distance-converter.py
+++ distance-converter.py
@@ -4,12 +4,23 @@ print("Enter units.")
 units = input()
 print("Enter target units.")
 tgt_units = input()
+
 mi = 1.0000
 km = 1.60934 * mi
 ft = 5280.0000 * mi
 m = 1609.34 * mi
 
-total_units = 
+if tgt_units == mi:
+    mi = 1.0000
+elif tgt_units == km:
+    km = 1.60934 * mi
+elif tgt_units == ft:
+    ft = 5280.0000 * mi
+elif tgt_units == m:
+    m = 1609.34 * mi
+
+
+total_units = float(distance) * float(tgt_units)
 
 if tgt_units == mi:
     print(distance + units + 'is' + total_units + tgt_units)
