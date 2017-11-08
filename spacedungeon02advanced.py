52e7ec8c5667dc369e8ec2363ee910c400466811
s203 <s203@AI241LAB0203M06.local>
2017-11-08 11:46:58 -0800

    updates

--- spacedungeon02advanced.py
+++ spacedungeon02advanced.py
@@ -1,11 +1,12 @@
+import random
 print("Attack Phase: Type in Spell to cast.")
 input()
 
 magic_power = 50
 
-while magic_power > 0:
+while magic_power >= 0:
     print("You casted a spell.")
-    magic_power -= 10
+    magic_power -= random.randint(1, 10)
     print("MP consumed. " + str(magic_power) + " MP left")
     print("Attack Phase: Type in Spell to cast.")
     input()
@@ -15,11 +16,11 @@ while magic_power > 0:
 print("Attack Phase: Type in Attack to perform.")
 input()
 
-hit_points = (50, 40, 30, 20, 10)
+hit_points = range(0, 50)
 current_hp = 50
 for current_hp in hit_points:
     print("Damage received.")
-    current_hp -= 10
+    current_hp -= random.randint(1, 10)
     print(str(current_hp) + " HP remaining.")
     print("Attack Phase: Type in Attack to perform.")
     input()
