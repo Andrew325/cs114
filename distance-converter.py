f76e7e351db78b1a35c960af3b1f986410238073
s203 <s203@AI241LAB0203M06.local>
2017-11-08 09:26:34 -0800

    updates

--- distance-converter.py
+++ distance-converter.py
@@ -1,12 +1,21 @@
-print("Enter a distance.")
-distance = int(input())
+print("Enter a distance(in numbers).")
+distance = input()
 print("Enter units.")
 units = input()
 print("Enter target units.")
 tgt_units = input()
-mi = 1
-units = mi
-km = mi * int(1.60934)
+mi = 1.0000
+km = 1.60934 * mi
+ft = 5280.0000 * mi
+m = 1609.34 * mi
 
-for units in distance:
-    print(str(units))
+total_units = 
+
+if tgt_units == mi:
+    print(distance + units + 'is' + total_units + tgt_units)
+elif tgt_units == km:
+    print(distance + units + 'is' + total_units + tgt_units)
+elif tgt_units == ft:
+    print(distance + units + 'is' + total_units + tgt_units)
+elif tgt_units == m:
+    print(distance + units + 'is' + total_units + tgt_units)
