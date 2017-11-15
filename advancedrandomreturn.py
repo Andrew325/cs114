f2968cb686b1fbcb5e03cc758528a0c0217004db
s203 <s203@2024-01-203-06.local>
2017-11-15 09:01:44 -0800

    updates

---
+++ advancedrandomreturn.py
@@ -0,0 +1,10 @@
+import random
+word = input("Type a word ")
+number = len(word)
+
+def random_number(random_word):
+    return wordnumb
+
+wordnumb = random.randint(0, number)
+answer = random_number(wordnumb)
+print(answer)
---
+++ randomreturn.py
@@ -0,0 +1,6 @@
+import random
+def random_number(returning_number):
+    return rn
+rn = random.randint(1, 20)
+answer = random_number(rn)
+print(answer)
